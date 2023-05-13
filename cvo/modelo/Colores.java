024ef546a9668544b4ce80a57edf436b8794bc26
Alvoid101 <10101010alvoid@gmail.com>
2023-05-13 18:27:15 -0500

    Clase colores

---
+++ cvo/modelo/Colores.java
@@ -0,0 +1,13 @@
+package cvo.modelo;
+
+import java.awt.Color;
+
+public class Colores {
+    //---Atributos---
+    public static Color ClASE = new Color(229, 56, 38);
+    public static Color OBJETO = new Color(235, 219, 178);
+    public static Color INSTANCIA = new Color(250, 186, 43);
+
+    public static Color RESALTADO = new Color(246, 245, 244);
+
+}
